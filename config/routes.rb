9a02f1fea3a2c75d2671b5500b62c0b3401653bc
cimigree <cimigreen@hotmail.com>
2018-11-22 15:43:02 -0500

    Adds models basic controllers and database schema and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :categories do
+    resources :items
+  end
+  resources :stores do
+    member do
+      get :all_items
+    end
+  end
+  resources :items do
+    collection do
+      get :items_all
+    end
+  end
 end
